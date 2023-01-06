d3da846fea2a9f4bacf5f6fa557b8fd81bba017f
Serge Konyakhin <sergekonyakhin@gmail.com>
2023-01-07 01:10:55 +0300

    Exercise_3 completed

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,58 @@
+﻿uint m;
+try
+{
+    m = GetNumber("Enter your number \"M\": ");
+}
+catch (FormatException ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+
+uint n;
+try
+{
+    n = GetNumber("Enter your number \"N\": ");
+}
+catch (FormatException ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+
+uint result = Recursion(m, n);
+Console.Write($"A(m, n) = {result}");
+
+
+
+uint Recursion(uint m, uint n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Recursion(m - 1, 1);
+    }
+    else
+    {
+        return Recursion(m - 1, Recursion(m, n -1));
+    }
+}
+
+
+
+uint GetNumber(string message)
+{
+    Console.Write(message);
+    bool isParsed = uint.TryParse(Console.ReadLine(), out uint number);
+    if (isParsed)
+    {
+        return number;
+    }
+    else
+    {
+        throw new FormatException("Invalid value entered!");
+    }
+}
